5623405e62a7397da2ccaac74cb22c7ff5cb9bb1
Aleksander <smokaleksander@gmail.com>
2019-05-14 02:25:08 +0200

    index and services pages content and linking

--- pages/views.py
+++ pages/views.py
@@ -5,4 +5,4 @@ def index(request):
     return render(request, 'pages/index.html')
 
 def about(request):
-    return render(request, 'pages/about.html')
+    return render(request, 'pages/services.html')
