4e5b4f934f7785a5975c6c23b3b3fd938c98e86a
Aleksander <smokaleksander@gmail.com>
2019-05-14 04:19:37 +0200

    create expert(worker) model

--- experts/models.py
+++ experts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Expert (models.Model):
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description =  models.TextField(blank=False)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=80)
+    hire_date= models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.name+' ' + self.lastname
