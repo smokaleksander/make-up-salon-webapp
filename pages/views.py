774dab85037ab5e793f2e84955e80ff716073bb7
Aleksander <smokaleksander@gmail.com>
2019-05-22 22:16:11 +0200

    dynamic workers listing on main page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from experts.models import Expert
+
 def index(request):
-    return render(request, 'pages/index.html')
+    experts=Expert.objects.all()
+    context={
+        'experts':experts
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/services.html')
